b9df4c0f3e8bee72496b058f561c4bcb19c41618
Himanshu Mishra <66140981+himanshu070@users.noreply.github.com>
2021-10-19 20:30:01 +0530

    updated

--- js/script.js
+++ js/script.js
@@ -3,10 +3,9 @@ const loadApi = async ()=> {
     let url = "https://www.omdbapi.com/?i=tt3896198&apikey=16a47919&t=";
     let data = await fetch(url+input.value);
     let parsedData = await data.json();
-    console.log(parsedData)
+    //console.log(parsedData)
 
     let poster = document.getElementById("poster");
-    poster.style.display = "block";
     let ratingString = ``
     for (let index = 0; index < parsedData.Ratings.length; index++) {
         ratingString+= parsedData.Ratings[index].Source + ` ` + parsedData.Ratings[index].Value + `<br>`   
@@ -15,11 +14,10 @@ const loadApi = async ()=> {
     poster.innerHTML = `<img class="poster-image" src=${parsedData.Poster} alt="poster">`
 
     let movieInformation = document.getElementById("movieInformation")
-    movieInformation.style.display = "block"
     movieInformation.innerHTML = `<div style='overflow:auto;' class="movieInformation-card"
     <h4><span class="movie-title">${parsedData.Title}</span> (${parsedData.Year})</h4>
     <br> 
-    <h4>⭐ ${parsedData.imdbRating} IMDb</h4>
+    <h4 class="imdb">⭐ ${parsedData.imdbRating} IMDb</h4>
     <br>
     <h4>Directed by ${parsedData.Director}</h4>
     <h4>${parsedData.Genre}</h4>
